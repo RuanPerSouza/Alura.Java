732fb08829952e701fb9afac88a0c1fc19ea4abd
Ruan P.Souza <ruanpereira020@gmail.com>
2026-04-20 20:52:41 -0300

    Update Main.java

--- src/br/com/alura/bytebank/Main.java
+++ src/br/com/alura/bytebank/Main.java
@@ -10,8 +10,17 @@ public class Main {
         boolean incluidoNoPlano = true;
         System.out.println("Incluido no plano: " + incluidoNoPlano);
         double notaDoFilme = 9.8;
-        System.out.println("Nota do Filme: " + notaDoFilme
-        );
+        System.out.println("Nota do Filme: " + notaDoFilme);
+        //Média calculada pelas vozes da minha cabeça
+        double media = (9.8 + 6.0 + 5) /3;
+        System.out.println("Media: " + media);
+        String sinopse;
+        sinopse = """
+            Filme de ficção cientifica no espaço cideral
+            Astronaltas explorando o universo
+            Imagems lindas do espaso 
+        """;
+        System.out.println(sinopse);
 
         
     }
